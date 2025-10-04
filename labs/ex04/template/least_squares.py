f8bd74b45c6424caea64a8b0a326dddeddd23201
abol0003 <95567171+abol0003@users.noreply.github.com>
2025-10-04 12:54:16 +0200

    add ex 4 solution

--- labs/ex04/template/least_squares.py
+++ labs/ex04/template/least_squares.py
@@ -6,6 +6,8 @@ Least Square
 
 import numpy as np
 
+from costs import *
+
 
 def least_squares(y, tx):
     """Calculate the least squares solution.
@@ -22,9 +24,10 @@ def least_squares(y, tx):
     >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
     (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
     """
-    # ***************************************************
-    # COPY YOUR CODE FROM EX03 HERE
-    # least squares: TODO
-    # returns optimal weights, MSE
-    # ***************************************************
-    raise NotImplementedError
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    w = np.linalg.solve(a, b) # w=a^-1 x b 
+    loss = compute_mse(y, tx, w)
+    # err=y-tx.dot(w)
+    # loss = np.mean(err**2)
+    return w, loss
